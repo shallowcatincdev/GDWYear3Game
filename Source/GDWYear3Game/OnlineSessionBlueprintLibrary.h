d6c8875a67714a8ce51c7ff7ae213054755ac2d3
Shallowcat <joshR364@hotmail.com>
2025-11-03 14:07:56 -0500

    unreal engine doesn't explode when C++ files are added anymore

---
+++ Source/GDWYear3Game/OnlineSessionBlueprintLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OnlineSessionBlueprintLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GDWYEAR3GAME_API UOnlineSessionBlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
