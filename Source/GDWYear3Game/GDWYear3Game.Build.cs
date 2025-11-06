e00d440015b7dd3c7590a9efddaf3c0e7fdf8f5f
Shallowcat <joshR364@hotmail.com>
2025-11-06 10:52:27 -0500

    EOS Working

--- Source/GDWYear3Game/GDWYear3Game.Build.cs
+++ Source/GDWYear3Game/GDWYear3Game.Build.cs
@@ -8,7 +8,7 @@ public class GDWYear3Game : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineServicesInterface", "OnlineSubsystemEOS", "OnlineSubsystemUtils"});
+        PublicDependencyModuleNames.AddRange(new string[] {"Core", "CoreUObject", "Engine", "InputCore"});
 
         PrivateDependencyModuleNames.AddRange(new string[] {  });
 
